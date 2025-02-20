d8ed7332f05fda0649c4100e83ac334a0e3d4548
Lexius Waltar <lexius.waltar@gmail.com>
2025-02-20 00:07:33 +0000

    Creates the activities

---
+++ array-exercises.js
@@ -0,0 +1,31 @@
+// array-exercises.js
+
+// Exercise 1: Create an array of three colors and print the first color.
+
+
+// Exercise 2: Add a color to the end of the array and print the array.
+
+
+// Exercise 3: Remove the last color from the array and print the array.
+
+
+// Exercise 4: Add a color to the beginning of the array and print the array.
+
+
+// Exercise 5: Remove the first color from the array and print the array.
+
+
+// Exercise 6: Print the number of colors in the array.
+
+
+// Exercise 7: Convert the array into a string separated by dashes.
+
+
+// Exercise 8: Replace the second color with "pink" and print the array.
+
+
+// Exercise 9: Create an array of numbers and print the third number.
+
+
+// Exercise 10: Remove the last element, add two numbers to the beginning, and print the array.
+
---
+++ practice-problem.js
@@ -0,0 +1,4 @@
+// You are throwing a party and have a list of attendees.
+// Someone new arrives—update your list.
+// Unfortunately, the last person on your list had to cancel—adjust your list accordingly.
+// Finally, check who is still attending and print the updated list.
